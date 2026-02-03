eb70a357c88d047b6f991f2d1f4052b5ae6efa5a
oluwaseun186 <adewumibode7@gmail.com>
2026-02-03 11:57:20 +0100

    dockerfile added

--- index.js
+++ index.js
@@ -2,80 +2,68 @@ require("dotenv").config();
 const { MongoClient } = require("mongodb");
 
 const express = require("express");
-let ejs = require('ejs');
-
+let ejs = require("ejs");
 
 // set the connection string
-const client = new MongoClient('mongodb+srv://testwitholu:4Klh5hx2ARYaDcXm@cluster100.0gfizy3.mongodb.net/?appName=Cluster100');
+const client = new MongoClient(
+  "mongodb+srv://testwitholu:4Klh5hx2ARYaDcXm@cluster100.0gfizy3.mongodb.net/?appName=Cluster100"
+);
 
 // path
 const path = require("path");
 
-// create a server 
-const server = express();
+// create a app
+const app = express();
 
-server.use(express.json());
+app.use(express.json());
 
-server.use(express.static("public"));
+app.use(express.static("public"));
 
-server.set("view engine", "ejs");
+app.set("view engine", "ejs");
 
+app.set("views", "./views");
 
-server.set("views", "./views");
+// ROutes for app
+app.get("/", function (request, response) {
+  //response.send("This is the home response")
+  //let indexPath = path.join(__dirname, "public", "index.html");
 
-
-// ROutes for server
-server.get("/", function(request, response){
-    //response.send("This is the home response")
-    //let indexPath = path.join(__dirname, "public", "index.html");
-
-    //console.log(indexPath);
-    //response.sendFile(indexPath);
-    response.render("index");
+  //console.log(indexPath);
+  //response.sendFile(indexPath);
+  response.render("index");
 });
 
-
-server.get("/about", function(request, response){
-
-    response.render("about")
-
+app.get("/about", function (request, response) {
+  response.render("about");
 });
 
-
-server.get("/contact", function(request, response){
-
-    response.render("contactus");
-
+app.get("/contact", function (request, response) {
+  response.render("contactus");
 });
 
-
-server.get("/register", function(request, response){
-
-    response.render("register");
-
-
+app.get("/register", function (request, response) {
+  response.render("register");
 });
 
+app.post("/register-user", async function (request, response) {
+  console.log(request.body);
 
-server.post("/register-user", async function(request, response){
+  await client
+    .db("testuser")
+    .collection("users")
+    .insertOne({ firstname: request.body.firstname });
 
-    console.log(request.body)
-
-    await client.db("testuser").collection("users").insertOne({ firstname: request.body.firstname });
-
-    response.send({
-        message: "Registration successful",
-        code: 'success'
-    });
-
-})
-
-server.get("/products", function(request, response){
-
-    response.send("This is product response");
-
+  response.send({
+    message: "Registration successful",
+    code: "success",
+  });
 });
 
+app.get("/products", function (request, response) {
+  response.send("This is product response");
+});
 
-// listen for requests 
-server.listen(process.env.PORT, () => console.log(`Server is listening on PORT: ${process.env.PORT}`) );
+// listen for requests
+app.listen(process.env.PORT, () =>
+  console.log(`Server is listening on PORT: ${process.env.PORT}`)
+);
